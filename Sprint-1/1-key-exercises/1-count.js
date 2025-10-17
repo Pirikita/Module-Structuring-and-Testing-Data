2d4e278301c22ae3132e1225d7ed1230e0823c1b
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-17 15:28:01 +0200

    Added variable to store initials Created index.html to test js file

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,7 @@ let count = 0;
 
 count = count + 1;
 
+//Line 3 is adding 1 to the current value. So, first we inform that count is equal to 0. Then we say that we want to add 1 to the count value
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
