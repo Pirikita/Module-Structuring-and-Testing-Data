8ac1f6b30d4397654c57b18113670a2682bd8f4e
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-18 15:56:47 +0200

    - create variable to store the dir part of the filePath variable and another to store the ext part of the variable

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -8,7 +8,5 @@ let lastName = "Johnson";
 let initials = firstName[0] + middleName[0] + lastName[0];
 console.log(initials);
 
-
-
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
