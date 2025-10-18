9a9cd22c2e76995689e8c4dde6aac29b91e7ff40
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-18 16:50:40 +0200

    -Through video tutorial and web research I learned how to make the console show random integer numbers within the range specified in the const -Logged random numbers genarated by the funcition to the console.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,17 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+console.log(num);
+
+// num represents a random integer number between the minimim (1) and a maximum (100) value. Includes both values.
+
+// Order in expressions is the first open and close brackets, meaning max-min+1 closes before math.floor
+// Then math.random is evaluated, which generates a random number between 0 and 1
+
+//documentation used:
+// https://www.youtube.com/watch?v=K2upGO5Bb48
+// ChatGPT
+// https://www.w3schools.com/js/js_math.asp
+
+// 81 ; 37 ; 94 ; 60 ; 72 ; 10 ; 32 ;
