a81b202837921f5914323e0639c94cd61e4c47a4
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-23 18:09:07 +0200

    - Guessed what will happen based on the original code; - tested the original code; confirmed prediction; - modified the code to fix issues; - tested the modified code to confirm it works as expected.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,30 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> write your prediction here - will not run because decimalNumber is declared twice
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+//function convertToPercentage(decimalNumber) {
+//  const decimalNumber = 0.5;
+//  const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
+//  return percentage;
+//}
 
-console.log(decimalNumber);
+//console.log(decimalNumber);
 
-// =============> write your explanation here
+// =============> write your explanation here - An error occur because we are declaring the decimalNumber variable twice.
+//we declare it in the function and in the first const.
+//we are also logging decimalNumber we should log the function 
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage() {
+  const decimalNumber = 0.5;
+  const percentage = `${decimalNumber * 100}%`; 
+
+  return percentage;
+}
+console.log(convertToPercentage());
