4bd3122434f5c865206fcb87b44d20c9c897eeaa
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-23 10:49:14 +0200

    the error is the order of the information. the const has to be declared before since it's used in the console.log statement.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// the error is the order of the information. 
+// the const has to be declared before since it's used in the console.log statement. 
