8b8c91c22dab41559369674f447a6f6377ad1421
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-23 10:47:30 +0200

    initially, the code had a "const" that means that is a fixed value and cannot be reassigned. the code wasn't running because of that. Changed from "const" to "let" to make it work.

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,10 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+
+console.log(age);
+
+// initially, the code had a "const" that means that is a fixed value and cannot be reassigned. 
+// the code wasn't running because of that. 
+// Changed from "const" to "let" to make it work. 
