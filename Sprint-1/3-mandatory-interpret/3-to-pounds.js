085042614605f1a4de97e02c232891e814becbd8
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-23 16:38:45 +0200

    Found this the most difficult so far. I need to practice more to understand better the strings. Used reading resources and youtube and AI to help me understand better.

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,21 +1,35 @@
-const penceString = "399p";
+const penceString = "5p";
 
+// here with without trailing p we are informing the program to remove the 'p' from the string. With substring (0, penceString.length - 1) 
+// we are telling the program to start from index 0 and go up to the length of the string minus 1 (which removes the last character 'p')
 const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
 );
 
+// here we are padding the string, meaning we are saying that we need 3 characters in total and if there's not 3 it should add 0's until
+// we have 3 characters. This is important for cases where we have less than 3 characters in the string
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+
+//here we are extracting the pounds part of the string by taking all characters from index 0 up to the length of the string
+// minus 2 (which gives us the last two characters for pence)
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
 );
-
+// here we are repeating the same process for pence, but this time we are taking the last two characters of the string
+// and padding it to ensure it has 2 characters
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
   .padEnd(2, "0");
 
+
+// here we declare that we want the output to be in the format of £ followed by pounds, a dot, and then pence
 console.log(`£${pounds}.${pence}`);
+
+// 1test = 399p = £3.99
+// 2test = 120p = £1.20
+// 3test = 5p = £0.05
 
 // This program takes a string representing a price in pence
 // The program then builds up a string representing the price in pounds
