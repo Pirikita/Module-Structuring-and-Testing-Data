bf2014f43f88f5472e728d04c69ba2737f1e2f8b
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-23 11:03:11 +0200

    Changed the name of const in order for it to start with letters and not numbers.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const HourClockTime12 = "08:53";
+const hourClockTime24 = "20:53";
+
+// we can't have have names starting with a number, but we can use it in the end.
