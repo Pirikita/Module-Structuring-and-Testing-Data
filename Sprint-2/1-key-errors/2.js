eb5fd6152c075e284507b185f6d1013e9471c7bf
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-26 13:12:16 +0100

    Adapted the code to run any number and return its square.

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,7 +3,7 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here - I believe that square is not valid because 3 is not a variable 
+// =============> write your prediction of the error here - I believe that square is not valid because 3 is not a valid variable 
 
 //ORIGINAL CODE
 //function square(3) {
@@ -13,7 +13,7 @@
 
 // =============> write the error message here - SyntaxError: Unexpected number
 
-// =============> explain this error message here - 
+// =============> explain this error message here - the number 3 is not a valid parameter name. It has to be a name and not a number.
 
 // Finally, correct the code to fix the problem
 
@@ -22,7 +22,7 @@
 function square(num) {
     return num * num;
 }
-
 console.log(square(38));
 
+//With this code, we can chose any number to square
 
