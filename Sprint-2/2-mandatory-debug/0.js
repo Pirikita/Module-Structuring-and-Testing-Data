316e8667e5a4e39e6550c5aa297946b453c32e40
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-28 15:30:29 +0100

    predicted the output and the error in the code. Explained the error and wrote the corrected code.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -6,7 +6,6 @@
 
 /*function multiply(a, b) {
   console.log(a * b);
-  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,27 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// The function sum is intended to return the sum of two numbers, a and b. there's an error in the return statement. 
+// And again it's returning undefined because the return statement is divided. 
+// Ouput is the sum and then undefined
 
-function sum(a, b) {
+/*function sum(a, b) {
   return;
   a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
+*/
 // =============> write your explanation here
+  // ERROR:The sum of 10 and 32 is undefined
+    // the return statement should have been written in one line as "return a + b;"
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 59 and 72 is ${sum(59 , 72)}`);
+
+// Test 1 : The sum of 10 and 32 is 42
+// Test 2 : The sum of 59 and 72 is 131
