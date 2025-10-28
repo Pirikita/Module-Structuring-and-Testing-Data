508bdf9f939357026724a9083f3d97eeeb71d09b
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-28 15:24:07 +0100

    predicted the output wrote return statement in function did 2 tests with different inputs

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,33 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+  // we have a function that multiplies two numbers and logs the result,
 
-function multiply(a, b) {
+
+/*function multiply(a, b) {
   console.log(a * b);
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+*/
 
 // =============> write your explanation here
+  //we have the console doing the multiplication and loggin it, but we 
+//are not returning anything from the funcntion. there's why the output is undefined.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  console.log(a * b);
+  return a * b;
+}
+
+console.log(`The result of multiplying 27 and 39 is ${multiply(27, 39)}`);
+
+// Test 1 answer: 
+// 320
+// The result of multiplying 10 and 32 is 320
+// Test 2 answer: 
+// 1053
+// The result of multiplying 27 and 39 is 1053
