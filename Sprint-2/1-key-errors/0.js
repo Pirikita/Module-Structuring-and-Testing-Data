18b1bcd0cff2dd73079aa0a0140bb0e92a74704b
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-28 15:10:10 +0100

    predicted and explain the problem wrote new code ran tests

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,21 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> we declare a variable str twice
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/*function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
-
-// =============> write your explanation here
+*/
+// =============> write your explanation here 
+// The error occurs because we are trying to declare a variable 'str' inside the function using 'let',
 // =============> write your new code here
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+
+console.log(capitalise("beatriz")); // Output: "Beatriz"
