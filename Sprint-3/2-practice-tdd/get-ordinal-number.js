051831d99318c6160acf456f1baddfef45f274b9
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-29 19:35:08 +0100

    updated docs with bugs re wrote code to get ordinal number tested ordinal number code

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,23 @@
-function getOrdinalNumber(num) {
-  return "1st";
-}
+
+function getOrdinalNumber(number) {
+    // Handle special cases 11th, 12th, 13th
+    if (number % 100 >= 11 && number % 100 <= 13) {
+        return number + "th";
+    }
+    
+    const lastDigit = number % 10; //to divide and see the last digit
+    
+    if (lastDigit === 1) {
+        return number + "st";
+    } else if (lastDigit === 2) {
+        return number + "nd";
+    } else if (lastDigit === 3) {
+        return number + "rd";
+    } else {
+        return number + "th";
+    }
+};
 
 module.exports = getOrdinalNumber;
+
+console.log(getOrdinalNumber(11)); 
