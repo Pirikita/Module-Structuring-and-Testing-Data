b5b6c3ad5a44e2127bcc03e2a311bfa10f2afa87
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-29 19:59:00 +0100

    created tests for different scenarios created function ran tests

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,15 +1,14 @@
 function countChar(str, char) {
   let count = 0;
   
-
+  // passes through every char of the string 
   for (let i = 0; i < str.length; i++) {
-  
+    // if the the actual char is the same as the searched on
     if (str[i] === char) {
-      count++; 
-    }
-  }  
+      count++; // add to the counter
+  }
   
-  return count;  
+  return count;
 }
 
 module.exports = countChar;
--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,13 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(str, count) {
+  
+  if (count < 0){
+    return "Error, can't repeat";
+  } 
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    result +=str;
+  }
+  return result;
 }
 
 module.exports = repeat;
