5fd11cbc9f1857ad405f6c7508981422f4a49d1a
Pirikita <beatriz.oliveira.work@gmail.com>
2025-10-31 11:01:05 +0100

    -Ran the function in the console and analyzed the output; - with help of reading materials and AI, understood how and what is doing; - explained the function and how it operates

--- Sprint-3/3-stretch/find.js
+++ Sprint-3/3-stretch/find.js
@@ -19,7 +19,19 @@ console.log(find("code your future", "z"));
 // Use the Python Visualiser to help you play computer with this example and observe how this code is executed
 // Pay particular attention to the following:
 
-// a) How the index variable updates during the call to find
-// b) What is the if statement used to check
-// c) Why is index++ being used?
-// d) What is the condition index < str.length used for?
+/* a) How the index variable updates during the call to find
+  It starts on the first character, if it doesn't match, increases the index by 1 and runs the test again until it finds the character
+  or it doesn't and in that case returns -1
+*/
+
+/* b) What is the if statement used to check
+    Is used to check if the character is equal to what we are looking for, if yes it returns the position and stops the function, if not,
+    it keeps running.
+*/
+
+/* c) Why is index++ being used?
+    If the first character doesn't match it increase the index by 1 and checks the next character.
+*/
+
+/* d) What is the condition index < str.length used for?
+    The condition is used to check the entire length of the string. Is checking every character from start to end. */
